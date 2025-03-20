8f497b256e182b433f7d1a188283ae7a0fbebf60
navaj bloch <navajbloch420@gmail.com>
2025-03-20 12:31:56 +0530

    changhe in frontend

--- Frontend/src/Pages/Product.jsx
+++ Frontend/src/Pages/Product.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../Context/ShopContext";
-import { assets } from "../assets/frontend_assets/assets";
 import RelatedProduct from "../Components/RelatedProduct";
 
 function Product() {
@@ -47,13 +46,7 @@ function Product() {
         {/* Product Details */}
         <div>
           <h1 className="text-3xl font-bold">{productData.name}</h1>
-          <div className="flex items-center gap-2 mt-2">
-            {[...Array(4)].map((_, i) => (
-              <img key={i} src={assets.star_icon} className="w-4" alt="Star" />
-            ))}
-            <img src={assets.star_dull_icon} className="w-4" alt="Star" />
-            <p className="text-gray-500 ml-2">(122 Reviews)</p>
-          </div>
+    
           <p className="text-2xl font-semibold text-gray-800 mt-3">{currency} {productData.price}</p>
           <p className="text-gray-600 mt-3 leading-relaxed">{productData.description}</p>
           <button onClick={() => addToCart(productData._id)} className="mt-5 px-6 py-3 bg-black text-white text-sm rounded-lg hover:bg-gray-800 transition">
@@ -67,21 +60,8 @@ function Product() {
         </div>
       </div>
 
-      {/* description and review section */}
-
-      <div className="mt-20">
-        <div className="flex ">
-          <b className="border px-5 py-3 text-sm">Description</b>
-          <p className="border px-5 py-3 text-sm">Review</p>
-        </div>
-
-        <div className="flex flex-col gap-4 border px-6 py-6 border-gray-400 text-sm text-black text-wrap">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum cum neque, dolor modi voluptatem quaerat ratione perspiciatis possimus nobis? Illum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aspernatur illo sed nihil quam corporis vitae pariatur unde commodi quasi!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum eligendi alias quae ab, voluptatibus quisquam omnis vero eos distinctio fugiat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. In modi eos nostrum dignissimos hic quis et adipisci sequi fugiat est!</p>
-        </div>
-           
-      </div>
-
+    
+   
       {/* display related product  */}
       <RelatedProduct category={productData.category} skinType={productData.skinType}/>
     </div>
